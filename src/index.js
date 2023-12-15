75bd8779623c99df1926f513b31ed84982459566
Sravan_Kumar_Samala <89454952+sravankumar-samala@users.noreply.github.com>
2023-12-15 20:46:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
-// import {NxtWatchContextProvider} from './context/GlobalContext'
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <NxtWatchContextProvider> */}
       <App />
-      {/* </NxtWatchContextProvider> */}
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
